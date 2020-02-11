a14aecf2ba92dbe2d03052e8b48f67f6487e337b
sang.nguyen <nguyentansang1996@gmail.com>
2020-02-11 08:10:57 +0700

    add: trans history function

---
+++ src/HomePage/TransHistory/TransactionHistory.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import { Table, Input, InputNumber, Form, Button, Icon, Tag, } from 'antd';
+
+const data = [];
+for (let i = 0; i < 5; i++) {
+  data.push({
+    transNumber: 234657 + i,
+    content: i % 2 === 0 ? 'Lương tháng' : 'Gửi lại tiền mượn',
+    amount: `${i % 2 === 0 ? '-' : '+'} ${5000000 * (i + 1)}`,
+    time: "12/12/2020",
+    transType: i % 2 === 0 ? 0 : 1,
+  });
+}
+
+const EditableContext = React.createContext();
+
+class TransactionHistoryForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data, editingKey: '' };
+    this.columns = [
+      {
+        title: 'Số giao dịch',
+        dataIndex: 'transNumber',
+        width: '25%',
+        editable: true,
+      },
+      {
+        title: 'Nội dung',
+        dataIndex: 'content',
+        width: '25%',
+        editable: true,
+      },
+      {
+        title: 'Số tiền',
+        dataIndex: 'amount',
+        width: '20%',
+        editable: true,
+      },
+      {
+        title: 'Loại giao dịch',
+        dataIndex: 'transType',
+        width: '15%',
+        editable: true,
+        render: (text, record) => {
+          switch (record.transType) {
+            case 0:
+              return <Tag color="red">Tiền ra</Tag>
+            case 1:
+              return <Tag color="green">Tiền vào</Tag>
+          }
+        }
+      },
+      {
+        title: 'Thời gian',
+        dataIndex: 'time',
+        width: '15%',
+        editable: true,
+      },
+    ];
+  }
+
+  render() {
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+        }),
+      };
+    });
+    return (
+      <EditableContext.Provider value={this.props.form}>
+        <Table
+          bordered
+          dataSource={this.state.data}
+          columns={columns}
+          rowClassName="editable-row"
+          pagination={{
+            onChange: this.cancel,
+          }}
+        />
+      </EditableContext.Provider>
+    );
+  }
+}
+
+const TransactionHistory = Form.create({name: 'validate_other'})(TransactionHistoryForm);
+
+export default TransactionHistory;
