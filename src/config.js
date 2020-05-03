43be0c75264853227b1c7f9759920caf46ecd6a3
Nguyen Sang <nguyentansang1996@gmail.com>
2020-05-04 00:07:37 +0700

    update config url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
-  //apiUrl: 'https://backend-bank-1.herokuapp.com/api/v1'
-  apiUrl: 'http://localhost:5000/api/v1',
-  socketUrl: 'http://localhost:5000'
+  apiUrl: ' https://api-bank-bbc.herokuapp.com/api/v1',
+  socketUrl: ' https://api-bank-bbc.herokuapp.com'
+  //apiUrl: 'http://localhost:5000/api/v1',
+  //socketUrl: 'http://localhost:5000'
 };
 
 export default config;
