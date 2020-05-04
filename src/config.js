1228eb6f40b5f30902129955d0825e4eaf0befb0
Nguyen Sang <nguyentansang1996@gmail.com>
2020-05-04 22:02:44 +0700

    change socket url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-  //apiUrl: ' https://api-bank-bbc.herokuapp.com/api/v1',
-  //socketUrl: '/'
-  apiUrl: 'http://localhost:5000/api/v1',
-  socketUrl: 'http://localhost:5000/'
+  apiUrl: ' https://api-bank-bbc.herokuapp.com/api/v1',
+  socketUrl: '/'
+  //apiUrl: 'http://localhost:5000/api/v1',
+  //socketUrl: 'http://localhost:5000/'
 };
 
 export default config;
